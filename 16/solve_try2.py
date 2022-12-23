8f30a0f5e04e32d83bda5151e6c4062467e97995
MustaphaEllouze <ellouze.mostafa@gmail.com>
2022-12-23 11:59:18 +0100

    Enigme16 partie 1 - Test Complete

--- 16/solve_try2.py
+++ 16/solve_try2.py
@@ -5,6 +5,10 @@ fic = open('input.txt')
 lines = fic.readlines()
 fic.close()
 
+# -------------------------- FUNCTIONS -------------------------
+def flatten(list):
+    return [elem  for sublist in list for elem in sublist]
+
 # -------------------------- CLASS -----------------------------
 class Valve:
     valves = []
@@ -26,6 +30,7 @@ class State:
     def __init__(self,states):
         self.states = states
         self.pressures = {key:0 for key in self.states.keys()}
+        self.opened = []
         self.final_state = 0
         self.curser=0
         self.current_valve=Valve.valves_dict['AA']
@@ -35,48 +40,53 @@ class State:
         self.pressures = {key:sum(Valve.valves_dict[key].flow*self.states[key]) for key in self.states.keys()}
         self.final_state = sum(self.pressures.values())
     
-    def possible_moves(self):
-        return Valve.valves_connect[self.current_valve]
-    
-    def can_open_valve(self):
-        return self.states[self.current_valve.name][self.curser]==0
-    
     def open_current_valve(self,useless_buffer):
         if(self.states[self.current_valve.name][self.curser]==1) : raise Exception('Already open')
         self.states[self.current_valve.name][self.curser+1:]=1
         self.curser+=1
+        self.opened.append(self.current_valve.name)
     
     def move_to_valve(self,valve):
         if(not valve in Valve.valves_connect[self.current_valve]) : raise Exception('Teleportation not allowed')
         self.current_valve=valve
         self.curser+=1
-    
-    def possible_actions(self):
-        part_1 = [('PM',valve) for valve in self.possible_moves()]
-        part_2 = [('OV',self.can_open_valve())]*self.can_open_valve()
-        return part_1+part_2
-    
-    def future_states(self):
-        possible_actions = self.possible_actions()
-        future_states = []
-        for action,argument in possible_actions:
-            fut_state = copy.copy(self)
-            fut_state.states = copy.deepcopy(self.states)
-            if action=='PM':
-                fut_state.path += 'Move to valve '+argument.name+' || '
-                fut_state.move_to_valve(argument)
-            elif(action=='OV'):
-                fut_state.path += 'Open valve '+fut_state.current_valve.name+' || '
-                fut_state.open_current_valve(argument)
-            fut_state.compute_pressure()
-            future_states.append(fut_state)
-        return future_states
 
     def construct_tree(self,iterations):
         if(iterations==1):
-            return self.future_states()
+            return self.interisting_moves()
         else:
-            return [state.construct_tree(iterations=iterations-1) for state in self.future_states()]
+            return flatten([state.construct_tree(iterations=iterations-1) for state in self.interisting_moves()])
+    
+    def how_to_move_to_valve(self,target):
+        visited = []
+        next_valve = self.current_valve
+        path = []
+        queue = [(valve,path+[valve.name]) for valve in Valve.valves_connect[next_valve]]
+        while (len(queue)!=0 and next_valve!=target):
+            next_valve,path = queue.pop(0)
+            queue += [(valve,path+[valve.name]) for valve in Valve.valves_connect[next_valve] if valve not in visited]
+            visited.append(next_valve)
+        return path
+    
+    def interisting_moves(self):
+        closed_valves = [valve for valve in Valve.valves if (valve.name not in self.opened and valve.flow>0)]
+        future_states = []
+        for valve in closed_valves:
+            path=self.how_to_move_to_valve(valve)
+            fut_state = copy.copy(self)
+            fut_state.states = copy.deepcopy(self.states)
+            fut_state.opened = copy.deepcopy(self.opened)
+            if len(path)+fut_state.curser<=29:
+                for name in path:
+                    fut_state.move_to_valve(Valve.valves_dict[name])
+                    fut_state.path += 'Move to valve '+name+' || '
+                fut_state.open_current_valve(0)
+                fut_state.path += 'Open valve '+fut_state.current_valve.name+' || '
+                fut_state.compute_pressure()
+                future_states.append(fut_state)
+        if(len(future_states)==0): return [self]
+        return future_states
+
     
 
 # -------------------------- PARSER ----------------------------
@@ -94,3 +104,13 @@ Valve.assemble()
 states = {valve.name:np.array([0]*30) for valve in Valve.valves}
 s = State(states)
 
+best_candidate = None
+best_score = -1
+
+all_states = {state:state.final_state for state in s.construct_tree(iterations=9)}
+
+sorted_states = sorted(all_states.items(),key= lambda item:item[1],reverse=True)
+
+best_state = sorted_states[0][0]
+
+print(best_state.path,best_state.final_state)
