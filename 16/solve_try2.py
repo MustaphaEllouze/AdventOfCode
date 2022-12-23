f430790b960e5bf67b405419f5d33f4c30ca8f1f
MustaphaEllouze <ellouze.mostafa@gmail.com>
2022-12-23 16:29:22 +0100

    Enigme16 partie 1 - Optimisation 10%

--- 16/solve_try2.py
+++ 16/solve_try2.py
@@ -1,9 +1,12 @@
 import numpy as np
 import copy
+import time
 
 fic = open('input2.txt')
 lines = fic.readlines()
 fic.close()
+
+t1 = time.time()
 
 # -------------------------- FUNCTIONS -------------------------
 def flatten(list):
@@ -24,10 +27,45 @@ class Valve:
         self.names = names
         Valve.valves.append(self)
         Valve.valves_dict[name]=self
-    def assemble():
+    
+    def compute_assemble():
+        v_connect = {}
+        n_connect = {}
         for valve in Valve.valves:
-            Valve.valves_connect[valve] = [Valve.valves_dict[valve_connected] for valve_connected in valve.names]
-            Valve.names_connected[valve.name] = [valve_connected for valve_connected in valve.names]
+            v_connect[valve] = {Valve.valves_dict[valve_connected]:1 for valve_connected in valve.names}
+            n_connect[valve.name] = {valve_connected:1 for valve_connected in valve.names}
+        return v_connect,n_connect
+
+    def assemble():
+        c = Valve.compute_assemble()
+        Valve.valves_connect = copy.copy(c[0])
+        Valve.names_connected = copy.copy(c[1])
+
+    def compute_assemble_optimized():
+        to_assemble = [valve for valve in Valve.valves if (valve.flow>0 or valve.name=='AA')]
+        v_connect,n_connect = Valve.compute_assemble()
+        v_o_connect = {}
+        n_o_connect = {}
+        for valve1 in to_assemble:
+            v_o_connect[valve1]={}
+            n_o_connect[valve1.name]={}
+            for valve2 in [v for v in to_assemble if not v is valve1]:
+                visited = []
+                next_valve = valve1
+                queue = [(valve,1) for valve in v_connect[next_valve]]
+                while (len(queue)!=0 and next_valve!=valve2):
+                    next_valve,depth = queue.pop(0)
+                    queue += [(valve,depth+1) for valve in v_connect[next_valve] if valve not in visited]
+                    visited.append(next_valve)
+                v_o_connect[valve1][valve2]=depth
+                n_o_connect[valve1.name][valve2.name]=depth
+        return v_o_connect,n_o_connect
+
+    def assemble_optimized():
+        c = Valve.compute_assemble_optimized()
+        Valve.valves_connect = copy.copy(c[0])
+        Valve.names_connected = copy.copy(c[1])                
+
 
 class State:
     def __init__(self,states):
@@ -50,9 +88,9 @@ class State:
         self.opened.append(self.current_valve.name)
     
     def move_to_valve(self,valve):
-        if(not valve in Valve.valves_connect[self.current_valve]) : raise Exception('Teleportation not allowed')
-        self.current_valve=valve
-        self.curser+=1
+        if(not valve in Valve.valves_connect[self.current_valve].keys()) : raise Exception('Teleportation not allowed')
+        self.curser+=Valve.valves_connect[self.current_valve][valve]
+        self.current_valve=valve        
 
     def construct_tree(self,iterations):
         if(iterations==1):
@@ -64,22 +102,23 @@ class State:
         visited = []
         next_valve = self.current_valve
         path = []
-        queue = [(valve,path+[valve.name]) for valve in Valve.valves_connect[next_valve]]
+        length = 0
+        queue = [(valve,path+[valve.name],length+Valve.valves_connect[next_valve][valve]) for valve in Valve.valves_connect[next_valve].keys()]
         while (len(queue)!=0 and next_valve!=target):
-            next_valve,path = queue.pop(0)
-            queue += [(valve,path+[valve.name]) for valve in Valve.valves_connect[next_valve] if valve not in visited]
+            next_valve,path,length = queue.pop(0)
+            queue += [(valve,path+[valve.name],length+Valve.valves_connect[next_valve][valve]) for valve in Valve.valves_connect[next_valve].keys() if valve not in visited]
             visited.append(next_valve)
-        return path
+        return path,length
     
     def interisting_moves(self):
         closed_valves = [valve for valve in Valve.valves if (valve.name not in self.opened and valve.flow>0)]
         future_states = []
         for valve in closed_valves:
-            path=self.how_to_move_to_valve(valve)
+            path,length=self.how_to_move_to_valve(valve)
             fut_state = copy.copy(self)
             fut_state.states = copy.deepcopy(self.states)
             fut_state.opened = copy.deepcopy(self.opened)
-            if len(path)+fut_state.curser<=29:
+            if length+fut_state.curser<=29:
                 for name in path:
                     fut_state.move_to_valve(Valve.valves_dict[name])
                     fut_state.path += 'Move to valve '+name+' || '
@@ -100,7 +139,8 @@ for line in lines:
     names = [elem[-2:] for elem in second_part.split(',')]
     Valve(name,int(fr),names)
 
-Valve.assemble()
+#Valve.assemble()
+Valve.assemble_optimized()
 
 # ------------------------- ALGORITHMIQUE ----------------------
 
@@ -110,12 +150,14 @@ s = State(states)
 best_candidate = None
 best_score = -1
 
-print(Valve.not_zero)
+iterations = 5
 
-all_states = {state:state.final_state for state in s.construct_tree(iterations=5)}
+all_states = {state:state.final_state for state in s.construct_tree(iterations=iterations)}
 
 sorted_states = sorted(all_states.items(),key= lambda item:item[1],reverse=True)
 
 best_state = sorted_states[0][0]
 
 print(best_state.path,best_state.final_state)
+
+print(f'Time spent = {(time.time()-t1)} s')
